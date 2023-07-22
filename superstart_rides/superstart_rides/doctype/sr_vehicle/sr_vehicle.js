f747e9407c47b03b22dbbf841f153d6c2838b2ac
Omar M. K. Shehada <o.shehada@ard.ly>
2023-07-22 22:26:44 +0200

    feat. init

---
+++ superstart_rides/superstart_rides/doctype/sr_vehicle/sr_vehicle.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Omar M. K. Shehada and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('SR Vehicle', {
+	// refresh: function(frm) {
+
+	// }
+});
